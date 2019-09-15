1e07ff437f6d86a48c966b69875f4688f6bfe3e5
MERGHOOB <margub.khan@gmail.com>
2019-09-15 15:12:54 +0530

    Step-10:introduction to Spring Data JPA More method on JPARepository

--- jpa-margub-steps/src/main/java/com/margub/learning/jpa/jpamargubsteps/UserRepositoryCommandLineRunner.java
+++ jpa-margub-steps/src/main/java/com/margub/learning/jpa/jpamargubsteps/UserRepositoryCommandLineRunner.java
@@ -8,6 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+import java.util.Optional;
+
 
 @Component
 public class UserRepositoryCommandLineRunner implements CommandLineRunner {
@@ -22,6 +25,13 @@ public class UserRepositoryCommandLineRunner implements CommandLineRunner {
     public void run(String... args) throws Exception {
         User user = new User("Jill", "Admin");
         userRepository.save(user);
+
         log.info("New User created: " + user);
+
+        Optional<User> userWithIdOne = userRepository.findById(1L);
+        log.info("User is retrivied " + userWithIdOne);
+
+        List<User> all = userRepository.findAll();
+        log.info("Users: "+ all);
     }
 }
