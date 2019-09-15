2bff813a5abb57623a20e64cd0fa69198de0c972
MERGHOOB <margub.khan@gmail.com>
2019-09-15 14:18:29 +0530

    Initialize the project

---
+++ jpa-margub-steps/src/main/java/com/margub/learning/jpa/jpamargubsteps/JpaMargubStepsApplication.java
@@ -0,0 +1,13 @@
+package com.margub.learning.jpa.jpamargubsteps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaMargubStepsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaMargubStepsApplication.class, args);
+	}
+
+}
---
+++ jpa-margub-steps/src/test/java/com/margub/learning/jpa/jpamargubsteps/JpaMargubStepsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.margub.learning.jpa.jpamargubsteps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaMargubStepsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
